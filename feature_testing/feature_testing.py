e8e1558267ca834bede14701fb256afae791ff68
Erik Hass <96741620+erh6479@users.noreply.github.com>
2022-05-20 10:45:38 +0200

    Update feature_testing.py

--- feature_testing/feature_testing.py
+++ feature_testing/feature_testing.py
@@ -17,18 +17,15 @@ def ExcelPrinter(filename):
     else:
         df.to_excel("{}.xls".format(filename))
 
-
-f1 = ({"Fixed": {"mean": 0}}, {"Normal": {"mean": 120, "stdev": 30, "min": 60, "max": 180}})
-f2 = ({"Normal": {"mean": 0.5, "stdev": 0.2, "min": 0.1, "max": 0.9}}, {"Fixed": {"mean": 1}})
-f3 = ({"Fixed": {"mean": 1}}, {"Normal": {"mean": 3460, "stdev": 1800, "min": 800, "max": 6000}})
-
 # Objects
 R = Repairman("R1", "Sascha")
 S = Source("S1", "Source", interArrivalTime={"Fixed": {"mean": 0.1}}, entity="manpy.Part", capacity=100)
 M1 = Machine("M1", "Machine1", processingTime={"Normal": {"mean": 0.2, "stdev": 0.1, "min": 0.08, "max": 0.34}})
+E1 = Exit("E1", "Exit1")
+
+# Features
 Ftr1 = Feature("Ftr1", "Feature1", victim=M1, deteriorationType="constant", distribution={"Time": {"Fixed": {"mean": 10}}, "Feature": {"Normal": {"mean": 120, "stdev": 30, "min": 60, "max": 180}}})
 Ftr2 = Feature("Ftr2", "Feature2", victim=M1, deteriorationType="working", distribution={"Time": {"Normal": {"mean": 0.5, "stdev": 0.2, "min": 0.1, "max": 0.9}}, "Feature": {"Fixed": {"mean": 1}}})
-E1 = Exit("E1", "Exit1")
 
 # Failures
 F1 = Failure(victim=M1, distribution={"TTF": {"Normal": {"mean": 120, "stdev": 45, "min": 60, "max": 200}}, "TTR": {"Normal": {"mean": 10, "stdev": 3, "min": 5, "max": 18}}}, repairman=R)
